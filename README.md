50190290544f79b7c843eb2459c6fb2723090167
Dima Zudochkin <dnd.pliz@gmail.com>
2014-01-20 20:32:56 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple application template
+
+* gem **slim-rails**
+* **HomeController** for root route
+* **README.rdoc** -> **README.md**
+* **:ru** is a default locale
+* added **config/database.yml** to **.gitignore**
+* changed generators (no stylesheets, no helpers, no javascripts while **rails generate**)
+* and initial commit
---
+++ template.rb
@@ -0,0 +1,42 @@
+s = <<EOS
+config.i18n.default_locale = :ru
+
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+      g.javascripts false
+    end
+EOS
+
+routes = <<EOS
+Rails.application.routes.draw do
+  root 'home#index'
+end
+EOS
+
+gem 'slim-rails'
+
+run 'bundle install'
+
+puts 'Added generators to config/application.rb'
+gsub_file 'config/application.rb', '# config.i18n.default_locale = :de', s
+
+create_file 'README.md', '# Simple rails application template'
+puts 'Added README.md file'
+
+remove_file 'README.rdoc'
+puts 'Removed README.rdoc file'
+
+generate(:controller, 'home index')
+
+remove_file 'config/routes.rb'
+create_file 'config/routes.rb', routes
+
+run 'git init'
+
+append_file '.gitignore', 'config/database.yml'
+
+run 'cp config/database.yml config/database.yml.example'
+
+run 'git add .'
+run 'git commit -m "Initial commit"'
